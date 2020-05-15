e985e6b1d298d222b41f414ac9d833ae0b3156ed
Wade <wsw0615@gmail.com>
2020-05-15 13:34:48 +0800

    feat: P1 seccess but fucking ugly

--- __test__/tennisKata.test.js
+++ __test__/tennisKata.test.js
@@ -1,7 +1,22 @@
 // Import 想執行測試的 JS 進來
 const testingJS = require('../src/tennisKata.js');
 
-test('測試基本斷言', () => {
-    expect(2 + 2).toBe(4);
-    expect(testingJS.funcSum(2, 3)).toBe(5);
-})
+test('love all', () => {
+    expect(testingJS.score(0, 0)).toBe('love all');
+});
+
+test('fifteen love', () => {
+    expect(testingJS.score(1, 0)).toBe('fifteen love');
+});
+
+test('thirty love', () => {
+    expect(testingJS.score(2, 0)).toBe('thirty love');
+});
+
+test('forty love', () => {
+    expect(testingJS.score(3, 0)).toBe('forty love');
+});
+
+test('Player one win', () => {
+    expect(testingJS.score(4, 0)).toBe('Player one win');
+});
--- src/tennisKata.js
+++ src/tennisKata.js
@@ -1,9 +1,19 @@
 console.log('Hello TennisKata');
 
-function sum (a, b){
-    return a + b
+function score (a, b){
+    if (a == 0 && b == 0){
+        return 'love all';
+    } else if (a == 1 && b == 0){
+        return 'fifteen love';
+    } else if (a == 2 && b == 0){
+        return 'thirty love';
+    } else if (a == 3 && b == 0){
+        return 'forty love';
+    } else if (a > 3 && a - b >= 2){
+        return 'Player one win';
+    }
 };
 
 module.exports = { //這個只在 node 環境下喔
-    funcSum: sum,
+    score: score
 }
