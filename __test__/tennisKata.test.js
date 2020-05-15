bacb582402e4b77656bc5ef3b26d460757b3f91b
Wade <wsw0615@gmail.com>
2020-05-15 12:08:23 +0800

    feat: import tennisKata.js and test it

--- __test__/tennisKata.test.js
+++ __test__/tennisKata.test.js
@@ -1,6 +1,7 @@
 // Import 想執行測試的 JS 進來
-require('../src/tennisKata.js');
+const testingJS = require('../src/tennisKata.js');
 
 test('測試基本斷言', () => {
     expect(2 + 2).toBe(4);
+    expect(testingJS.funcSum(2, 3)).toBe(5);
 })
--- src/tennisKata.js
+++ src/tennisKata.js
@@ -1,1 +1,9 @@
 console.log('Hello TennisKata');
+
+function sum (a, b){
+    return a + b
+};
+
+module.exports = { //這個只在 node 環境下喔
+    funcSum: sum,
+}
