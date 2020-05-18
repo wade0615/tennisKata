c3dfe4bdb7e47555e068f0b5d39e62299b3dd7f0
Wade <wsw0615@gmail.com>
2020-05-18 13:34:06 +0800

    feat: done

--- _test_/test.test.js
+++ _test_/test.test.js
@@ -9,3 +9,14 @@ test('deuce', () => {
     expect(testingJS.score(2, 2)).toBe('deuce');
     expect(testingJS.score(3, 3)).toBe('deuce');
 });
+
+test('at point', () => {
+    expect(testingJS.score(1, 0)).toBe('fifteen love');
+    expect(testingJS.score(2, 0)).toBe('thirty love');
+    expect(testingJS.score(3, 0)).toBe('forty love');
+});
+
+test('winner', () => {
+    expect(testingJS.score(4, 2)).toBe('p1 win');
+    expect(testingJS.score(3, 5)).toBe('p2 win');
+});
--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ function score (p1, p2){
     else if (isDeuce(p1, p2)){
         return 'deuce'
     }
+    else if (atPoint(p1, p2)){
+        return `${playerState[p1]} ${playerState[p2]}`;
+    }
+    else if (hasWinner(p1, p2)){
+        return `${whoWins(p1, p2)} win`;
+    }
 };
 
 function isAtAll (p1, p2){
@@ -15,6 +21,18 @@ function isAtAll (p1, p2){
 
 function isDeuce(p1, p2){
     return (p1 === p2) && (p1 > 1) ? true : false;
+};
+
+function atPoint(p1, p2){
+    return (p1 != p2) && (p1 <= 3) && (p2 <= 3) ? true : false;
+};
+
+function hasWinner(p1, p2){
+    return (p1 || p2 > 3) && (Math.abs(p1 - p2) > 1) ? true : false;
+};
+
+function whoWins(p1, p2){
+    return (p1 > p2) ? 'p1' : 'p2';
 }
 
 module.exports = {
