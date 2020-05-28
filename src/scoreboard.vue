de8bf6c17e5b4ada291bad2916b563e61e86a0c1
Wade <wsw0615@gmail.com>
2020-05-28 15:37:21 +0800

    feat: vue spa

---
+++ src/scoreboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="title">{{ msg }}</div>
+</template>
+
+<script>
+module.exports = {
+    data () {
+        return {
+            msg: "Let's start the game."
+        }
+    }
+}
+</script>
+
+<style>
+.title {
+    color: red;
+}
+</style>
